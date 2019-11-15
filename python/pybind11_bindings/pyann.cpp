8308d0d9453dd8640ca9bc6c2c64693f1bb4e02d
Guangning Tan <guangning.tan@mujin.co.jp>
2019-11-15 16:03:01 +0900

    append comma "," in the macro PY_ARGS for consistency

--- python/pybind11_bindings/pyann.cpp
+++ python/pybind11_bindings/pyann.cpp
@@ -27,6 +27,7 @@
 
 #define OPENRAVE_BININGS_PYARRAY
 #include "bindings.h"
+#include "docstrings.h"
 
 #include <ANN/ANN.h>
 
@@ -34,8 +35,6 @@
 #ifdef USE_PYBIND11_PYTHON_BINDINGS
 namespace py = pybind11;
 #else
-// cannot get from bindings.h, so have to redeclare them here
-#define PY_ARGS(...) py::args(__VA_ARGS__)
 namespace py = boost::python;
 #endif // USE_PYBIND11_PYTHON_BINDINGS
 using py::object;
@@ -357,12 +356,12 @@ OPENRAVE_PYTHON_MODULE(pyANN_int)
 #endif
     .def("__del__", &destroy_points)
 
-    .def("kSearch", &ksearch,PY_ARGS("q","k","eps"))
-    .def("kSearchArray", &ksearch_array,PY_ARGS("q","k","eps"))
-    .def("kPriSearch", &k_priority_search,PY_ARGS("q","k","eps"))
-    .def("kPriSearchArray", &k_priority_search_array,PY_ARGS("q","k","eps"))
-    .def("kFRSearch", &k_fixed_radius_search,PY_ARGS("q","sqrad","k","eps"))
-    .def("kFRSearchArray", &k_fixed_radius_search_array,PY_ARGS("qarray","sqrad","k","eps"))
+    .def("kSearch", &ksearch, PY_ARGS("q", "k", "eps") DOXY_FN1(ksearch))
+    .def("kSearchArray", &ksearch_array, PY_ARGS("q", "k", "eps") DOXY_FN1(kSearchArray))
+    .def("kPriSearch", &k_priority_search, PY_ARGS("q","k","eps") DOXY_FN1(kPriSearch))
+    .def("kPriSearchArray", &k_priority_search_array, PY_ARGS("q", "k", "eps") DOXY_FN1(kPriSearchArray))
+    .def("kFRSearch", &k_fixed_radius_search, PY_ARGS("q", "sqrad", "k", "eps") DOXY_FN1(kFRSearch))
+    .def("kFRSearchArray", &k_fixed_radius_search_array, PY_ARGS("qarray", "sqrad", "k", "eps") DOXY_FN1(kFRSearchArray))
 
     .def("__len__",             &ANNkd_tree::nPoints)
     .def("dim",                 &ANNkd_tree::theDim)
